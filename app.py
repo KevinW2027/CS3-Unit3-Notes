62e2d3e59e649c4c8f71c4ec2b66693bf77aaf25
KevinW2027 <kevin.wang2027@gbwl.org>
2026-02-11 14:37:41 +0000

    commit

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 #using the app.route decorartor to map the URL
 @app.route("/")
 def index():
-    name_data = None
-    return render_template("index.html", name=name_data)
+    name_data = 'Kevin'
+    year_data = 2027
+    favorite_list = ['apple','banana','pear','peach']
+    rating_dict = {
+        'apple': 7,
+        'banana': 5,
+        'pear': 9,
+        'peach': 10
+        }
+
+    return render_template("index.html", name=name_data, year=year_data, favorites=favorite_list, ratings=rating_dict)
 
 #TO RUN YOUR APP enter "flask run" into your terminal
 #TO STOP press ctrl+c in the terminal
